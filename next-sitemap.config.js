32faaa454440d51620b0995910ede9e75de74d07
dsa9925 <dev.dsa9925@gmail.com>
2024-03-26 12:48:26 +0900

    검색엔진 등록 수정

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://health-center-devgreact.vercel.app",
+  siteUrl: process.env.SITE_URL || "https://kkobrain-topaz.vercel.app",
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
